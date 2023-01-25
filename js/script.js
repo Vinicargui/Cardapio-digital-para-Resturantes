399fc3822eeb99322cbb95b932296a9cefda668a
vinicius <vinicius.cardoso8888@outlook.com>
2023-01-24 23:53:03 -0300

    alterando arquivo js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
-const btn = document.querySelectorAll(".botoes");
-const pratos = document.querySelector(".pratos");
+const btns = document.querySelectorAll(".btn");
+const sections = document.querySelectorAll("section");
 
-btn.forEach((e)=>{
-        e.addEventListener("click",()=>{
-            pratos.setAttribute(style.display = "block")
-        })
-})
+// btns.forEach((e)=>{
+//         e.addEventListener("click",()=>{
+//             pratos.setAttribute(style.display = "block")
+//         })
+// })
+
+
+btns.forEach((btn, idx) => {
+    // você adicionou um evento de clique aqui
+    btn.addEventListener('click', () => {
+      // eu chamei a nodeList das sections e para cada section eu fiz uma lógica
+      sections.forEach(section => {
+        // se a section já estiver visível, ela sai de tela
+        if (section.style.display === 'grid') {
+          section.style.display = 'none';
+        }
+        // eu deixo visível o bloco equivalente ao botão clicado
+        sections[idx].style.display = 'grid';
+      });
+    });
+  });
